3358bbcc21d6a6bb61d71d2559bcb75ab9d24c7f
Ajay <ajay.summer2@gmail.com>
2021-04-21 17:50:05 +0100

    Add message based on score in results screen

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,23 @@
 const Results = ({ numberOfQuestions, numberCorrect, handlePlayAgain }) => {
+  const getResultsMessage = () => {
+    const percent = numberCorrect / numberOfQuestions * 100
+    if (percent < 40) {
+      return "It's ok, you're just having a bad day."
+    } else if (percent < 70) {
+      return "Not bad, you just need to brush up a bit."
+    } else if (percent < 100) {
+      return "Well done! You've accumulated quite a bit of knowledge."
+    } else {
+      return "Perfect! Perhaps try another category and aim for glory."
+    }
+  }
+
   return (
     <div className="text-center border rounded-lg my-4">
       <h4 className="my-3">Thanks for playing!</h4>
       <h4>Final score:</h4>
       <h1>{numberCorrect}/{numberOfQuestions}</h1>
+      <h4>{getResultsMessage()}</h4>
       <div className="bg-white rounded-pill mx-5 my-4">
         <button
           type="button"
